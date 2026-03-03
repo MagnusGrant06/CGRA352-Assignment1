be5f6aa379d4055e5e25606b49d13ad266e1f861
Magnus Grant <grantmagn@ecs.vuw.ac.nz>
2026-03-03 15:25:54 +1300

    Completed core

--- CGRA352-Assignment1/CGRA352-Assignment1.cpp
+++ CGRA352-Assignment1/CGRA352-Assignment1.cpp
@@ -6,13 +6,139 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
+void part1() {
+
+    std::string img_path = "Flower.jpg";
+    cv::Mat BGR_flower = cv::imread(img_path);
+
+    cv::Mat HSV_flower = cv::imread(img_path);
+
+    cv::cvtColor(BGR_flower, HSV_flower, cv::COLOR_BGR2HSV);
+
+    cv::Mat combined;
+    cv::hconcat(BGR_flower, HSV_flower, combined);
+    cv::imshow("BGR and HSV", combined);
+
+    cv::waitKey(0);
+
+    //initialize grayscale images
+    cv::Mat BGR_B = cv::imread(img_path);
+    cv::cvtColor(BGR_flower, BGR_B, cv::COLOR_BGR2GRAY);
+    
+    cv::Mat BGR_G = BGR_B.clone();
+    cv::Mat BGR_R = BGR_B.clone();
+
+    cv::Mat HSV_H = BGR_B.clone();
+    cv::Mat HSV_S = BGR_B.clone();
+    cv::Mat HSV_V = BGR_B.clone();
+
+    for (int i = 0; i < BGR_flower.rows; i++) {
+        for (int j = 0; j < BGR_flower.cols; j++) {
+            cv::Vec3b current_BGR_pixel = BGR_flower.at<cv::Vec3b>(i, j);
+            BGR_B.at<uchar>(i, j) = current_BGR_pixel.val[0];
+            BGR_G.at<uchar>(i, j) = current_BGR_pixel.val[1];
+            BGR_R.at<uchar>(i, j) = current_BGR_pixel.val[2];
+
+            cv::Vec3b current_HSV_pixel = HSV_flower.at<cv::Vec3b>(i, j);
+            HSV_H.at<uchar>(i, j) = current_HSV_pixel.val[0];
+            HSV_S.at<uchar>(i, j) = current_HSV_pixel.val[1];
+            HSV_V.at<uchar>(i, j) = current_HSV_pixel.val[2];
+        }
+    }
+
+    cv::Mat HSV_row ,BGR_row, final_combined;
+    cv::hconcat(BGR_B, BGR_G, BGR_row);
+    cv::hconcat( BGR_row, BGR_R, BGR_row);
+
+    cv::hconcat(HSV_H, HSV_S, HSV_row);
+    cv::hconcat(HSV_row, HSV_V, HSV_row);
+
+    cv::vconcat(BGR_row, HSV_row, final_combined);
+    cv::imshow("Part 1",final_combined);
+
+    cv::waitKey(0);
+}
+
+void part2() {
+
+    std::string img_path = "Flower.jpg";
+    cv::Mat BGR_flower = cv::imread(img_path);
+
+    cv::Mat HSV_flower = cv::imread(img_path);
+
+    cv::cvtColor(BGR_flower, HSV_flower, cv::COLOR_BGR2HSV);
+
+
+    cv::Mat combined;
+    for (float intensity = 0.0; intensity < 1; intensity += 0.2) {
+        cv::Mat vert_combined;
+        for (int i = 0; i < 3; i++) {
+
+            cv::Mat temp_channels[3];
+            cv::split(HSV_flower, temp_channels);
+
+            temp_channels[i] = temp_channels[i] * intensity;
+
+            cv::Mat result;
+            cv::merge(temp_channels, 3, result);
+
+            cv::Mat result_BGR;
+            cv::cvtColor(result, result_BGR, cv::COLOR_HSV2BGR);
+
+
+            if (vert_combined.empty()) {
+                vert_combined = result_BGR.clone();
+            }
+            else {
+                cv::vconcat(vert_combined, result_BGR, vert_combined);
+            }
+        }
+
+        if (combined.empty()) {
+            combined = vert_combined.clone();
+        }
+        else {
+            cv::hconcat(combined, vert_combined, combined);
+        }
+
+    }
+
+    cv::imshow("test", combined);
+
+    cv::waitKey(0);
+}
+
+void part3() {
+
+    std::string img_path = "Flower.jpg";
+    cv::Mat BGR_flower = cv::imread(img_path);
+    
+    cv::Vec3b base_pixel = BGR_flower.at<cv::Vec3b>(80, 80);
+
+    for (int i = 0; i < BGR_flower.rows; i++) {
+        for (int j = 0; j < BGR_flower.cols; j++) {
+            cv::Vec3b& target_pixel = BGR_flower.at<cv::Vec3b>(i, j);
+
+            if (cv::norm(base_pixel, target_pixel) < 100.0) {
+                target_pixel = cv::Vec3b(255, 255, 255);
+            }
+            else {
+                target_pixel = cv::Vec3b(0, 0, 0);
+            }
+        }
+    }
+
+    cv::imshow("test",BGR_flower);
+
+    cv::waitKey(0);
+    
+}
+
 int main()
 {
-    std::string img_path = "c:/Users/magnu/Downloads/green_turtle_talisman_talisman_elden_ring_wiki_guide_200px.png";
-    cv::Mat mat = cv::imread(img_path);
-    
-    cv::imshow("Image", mat);
-    cv::waitKey(0);
+    part1();
+    part2();
+    part3();
 
     return 0;
 }
